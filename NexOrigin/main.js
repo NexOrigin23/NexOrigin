0c0b43206fd62f2dc17541d5c03ea64b17868b5d
Nex Origin <145690646+NexOrigin23@users.noreply.github.com>
2023-09-23 15:24:56 +0530

    Add files via upload

--- NexOrigin/main.js
+++ NexOrigin/main.js
@@ -2,6 +2,12 @@ darkMode();
 
 var navLinks = document.getElementById("Links");
 
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load",function(){
+  loader.style.display ="none";
+})
+
 function showMenu() {
   navLinks.style.right = "0";
   navLinks.style.display = "block";
@@ -15,32 +21,4 @@ function hideMenu() {
 function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
-}
-
-let dubaiText = document.getElementById('dubaitext');
-
-let burjkhalifa = document.getElementById('burjkhalifa');
-
-let stars = document.getElementById('stars');
-
-window.addEventListener('scroll', () => {
-  let value = window.scrollY;
-  dubaiText.style.left =value * -2 + 'px';
-  burjkhalifa.style.top =value * 1 + 'px';
-})
-
-const enchace = id =>{
-  const element = document.getElementById(id),
-  text = element.innerHTML.split("");
-
-  element.innerHTML = "";
-
-  text.forEach(letter=>{
-    const span = document.createElement("span");
-
-    span.className="letter";
-
-    span.innerHTML =letter;
-    element.appendChild(span);
-  });
 }
